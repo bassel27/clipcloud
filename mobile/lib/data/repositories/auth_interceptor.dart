73aa3196b1844fee069bb962a5ed4a0a761f8f8b
bassel27 <bassel_sabour@hotmail.com>
2025-07-23 13:43:19 +0300

    improve file structure in mobile

---
+++ mobile/lib/data/repositories/auth_interceptor.dart
@@ -0,0 +1,85 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:dio/dio.dart';
+import '../services/auth_service.dart';
+
+class DioFactory {
+  final Dio dio;
+
+  DioFactory({required String baseUrl, required AuthService authService})
+    : dio = Dio(BaseOptions(baseUrl: baseUrl)) {
+    dio.interceptors.add(AuthInterceptor(authService));
+  }
+}
+
+class AuthInterceptor extends Interceptor {
+  final AuthService authService;
+
+  AuthInterceptor(this.authService);
+
+  @override
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final headers = await authService.getAuthHeaders();
+    options.headers.addAll(headers);
+    handler.next(options);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    if (_shouldRetryWithRefresh(err)) {
+      try {
+        await authService.refreshTokens();
+        final response = await _retryRequest(err);
+        handler.resolve(response);
+        return;
+      } catch (refreshError) {
+        print('Token refresh failed: $refreshError');
+      }
+    }
+    handler.next(err);
+  }
+
+  bool _shouldRetryWithRefresh(DioException err) {
+    return err.response?.statusCode == 401 || err.response?.statusCode == 403;
+  }
+
+  Future<Response> _retryRequest(DioException err) async {
+    final newHeaders = await authService.getAuthHeaders();
+    final options = err.requestOptions;
+
+    return Dio().fetch(
+      RequestOptions(
+        path: options.path,
+        method: options.method,
+        baseUrl: options.baseUrl,
+        headers: {...options.headers, ...newHeaders},
+        queryParameters: options.queryParameters,
+        data: _isUploadRequest(options)
+            ? await _recreateFormData(options)
+            : options.data,
+        connectTimeout: options.connectTimeout,
+        receiveTimeout: options.receiveTimeout,
+        sendTimeout: options.sendTimeout,
+        responseType: options.responseType,
+        extra: options.extra,
+      ),
+    );
+  }
+
+  bool _isUploadRequest(RequestOptions options) {
+    return options.extra['isUpload'] == true;
+  }
+
+  Future<FormData> _recreateFormData(RequestOptions options) async {
+    final title = options.extra['title'] as String;
+    final filePath = options.extra['filePath'] as String;
+
+    return FormData.fromMap({
+      'title': title,
+      'media': await MultipartFile.fromFile(filePath),
+    });
+  }
+}
--- mobile/lib/data/repositories/media_repository.dart
+++ mobile/lib/data/repositories/media_repository.dart
@@ -1,86 +1,24 @@
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:dio/dio.dart';
-import 'package:mobile/data/models/media.dart';
-import 'package:mobile/data/services/auth_service.dart';
+import 'package:mobile/data/repositories/auth_interceptor.dart';
+import '../models/media.dart';
+import '../services/auth_service.dart';
 
 class MediaRepository {
   final Dio _dio;
-  final AuthService authService;
 
-  MediaRepository({required String baseUrl, required this.authService})
+  MediaRepository({required String baseUrl, required AuthService authService})
     : _dio = Dio(BaseOptions(baseUrl: baseUrl)) {
-    _dio.interceptors.add(
-      InterceptorsWrapper(
-        onRequest: (options, handler) async {
-          final headers = await authService.getAuthHeaders();
-          options.headers.addAll(headers);
-          handler.next(options);
-        },
-        onError: (error, handler) async {
-          if (error.response?.statusCode == 401 ||
-              error.response?.statusCode == 403) {
-            try {
-              await authService.refreshTokens();
-              final newHeaders = await authService.getAuthHeaders();
-
-              // Check if this is an upload request that needs FormData recreation
-              if (error.requestOptions.extra['isUpload'] == true) {
-                final title = error.requestOptions.extra['title'] as String;
-                final filePath =
-                    error.requestOptions.extra['filePath'] as String;
-
-                final newFormData = FormData.fromMap({
-                  'title': title,
-                  'media': await MultipartFile.fromFile(filePath),
-                });
-
-                final newOptions = RequestOptions(
-                  path: error.requestOptions.path,
-                  method: error.requestOptions.method,
-                  baseUrl: error.requestOptions.baseUrl,
-                  headers: {...error.requestOptions.headers, ...newHeaders},
-                  data: newFormData,
-                  extra: error.requestOptions.extra,
-                );
-
-                final response = await _dio.fetch(newOptions);
-                handler.resolve(response);
-              } else {
-                // For non-upload requests, use the original approach
-                final newOptions = RequestOptions(
-                  path: error.requestOptions.path,
-                  method: error.requestOptions.method,
-                  baseUrl: error.requestOptions.baseUrl,
-                  headers: {...error.requestOptions.headers, ...newHeaders},
-                  queryParameters: error.requestOptions.queryParameters,
-                  data: error.requestOptions.data,
-                  connectTimeout: error.requestOptions.connectTimeout,
-                  receiveTimeout: error.requestOptions.receiveTimeout,
-                  sendTimeout: error.requestOptions.sendTimeout,
-                  responseType: error.requestOptions.responseType,
-                );
-
-                final response = await _dio.fetch(newOptions);
-                handler.resolve(response);
-              }
-            } catch (refreshError) {
-              print('Token refresh failed: $refreshError');
-              handler.next(error);
-            }
-          } else {
-            handler.next(error);
-          }
-        },
-      ),
-    );
+    _dio.interceptors.add(AuthInterceptor(authService));
   }
 
   Future<List<Media>> getAllMedia() async {
     try {
       final response = await _dio.get('/media');
-      final List<dynamic> data = response.data;
-      return data.map((json) => Media.fromJson(json)).toList();
+      return (response.data as List)
+          .map((json) => Media.fromJson(json))
+          .toList();
     } on DioException catch (e) {
       throw Exception('Failed to fetch media: ${e.message}');
     }
@@ -88,17 +26,14 @@ class MediaRepository {
 
   Future<Media> uploadMedia({required String title, required File file}) async {
     try {
-      // Store file info for potential retry
-      final filePath = file.path;
-
       final response = await _dio.post(
         '/media/upload',
         data: FormData.fromMap({
           'title': title,
-          'media': await MultipartFile.fromFile(filePath),
+          'media': await MultipartFile.fromFile(file.path),
         }),
         options: Options(
-          extra: {'isUpload': true, 'title': title, 'filePath': filePath},
+          extra: {'isUpload': true, 'title': title, 'filePath': file.path},
         ),
       );
       return Media.fromJson(response.data);
